d42696ca6cd664fcb2f36eebb527b1449537847e
circleghost <circleghost0723@gmail.com>
2019-03-02 16:27:25 +0800

    add filter

--- read.py
+++ read.py
@@ -10,3 +10,9 @@ sum_len = 0
 for d in data:
 	sum_len = sum_len + len(d)
 print('平均有', sum_len/len(data), '筆數量')
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('總共有', len(new), '筆留言字數小於100')
